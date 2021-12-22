287b728900074c80c0b3b1ede4ad651bd3222771
daisy <181250102@smail.nju.edu.cn>
2021-12-22 15:18:01 +0800

    fix:

--- frontend/src/util/axios.js
+++ frontend/src/util/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const service = axios.create({
     // baseURL: "http://127.0.0.1:4523/mock/476309/"
-    baseURL: "http://118.195.232.175:8080/anigraph-backend"
+    baseURL: "localhost:8080"
 });
 
 service.interceptors.request.use((config) => {
